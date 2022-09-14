efcaa88322dc3a9c45db250ca4c0ab0460c4f6ba
anshi06 <araghya6768@gmail.com>
2022-09-14 09:36:02 +0530

    Header, HeaderCart

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import MealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={MealsImage} alt="A table full of delicious food " />
+      </div>
+    </>
+  );
+};
+export default Header;
