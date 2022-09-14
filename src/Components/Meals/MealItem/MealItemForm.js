1fbd13318a56fc524a9f779f056e8615c52fc1e2
anshi06 <araghya6768@gmail.com>
2022-09-14 11:20:45 +0530

    Input componenet, MealItemForm

---
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,18 @@
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
+const MealItemForm = (props) =>{
+    return (
+        <form className={classes.form}>
+            <Input label="Amount" input={{
+                id:"amount",
+                type:"number",
+                min:"1",
+                max:'5',
+                default:'1'
+            }}/>
+            <button>+ Add</button>
+        </form>
+    );
+
+}
+export default MealItemForm;
